04d33fb05e29a2156ffce995e58c85a52716981b
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2024-02-27 03:18:39 +0900

    feat: add unionfind

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod lazysegtree;
 pub mod multiset;
 pub mod persistent;
 pub mod segtree;
+pub mod unionfind;
---
+++ src/unionfind.rs
@@ -0,0 +1,78 @@
+/// A struct to manage disjoint sets.
+pub struct UnionFind {
+    parent: Vec<Option<usize>>,
+    size: Vec<usize>,
+    n: usize,
+}
+
+impl UnionFind {
+    /// Create a `UnionFind` with size `n`.
+    pub fn new(n: usize) -> Self {
+        let parent = vec![None; n];
+        let size = vec![1; n];
+        Self { parent, size, n }
+    }
+
+    /// Returns the root element of the group to which the element belongs.
+    pub fn root(&mut self, x: usize) -> usize {
+        assert!(x < self.n);
+        match self.parent[x] {
+            Some(parent) => {
+                let res = self.root(parent);
+                self.parent[x] = Some(res);
+                res
+            }
+            None => x,
+        }
+    }
+
+    /// Returns true if the two elements is in same group.
+    pub fn is_same(&mut self, x: usize, y: usize) -> bool {
+        assert!(x < self.n && y < self.n);
+        self.root(x) == self.root(y)
+    }
+
+    /// Merge two element. If the two elemnts is already in same group, return false.
+    /// Otherwise return true.
+    pub fn merge(&mut self, x: usize, y: usize) -> bool {
+        assert!(x < self.n && y < self.n);
+        let rx = self.root(x);
+        let ry = self.root(y);
+        if rx == ry {
+            return false;
+        }
+        // union by size
+        if self.size[rx] < self.size[ry] {
+            self.parent[rx] = Some(ry);
+            self.size[ry] += self.size[rx];
+        } else {
+            self.parent[ry] = Some(rx);
+            self.size[rx] += self.size[ry];
+        }
+        true
+    }
+
+    /// Returns the size of group to which the element belongs.
+    pub fn size(&self, x: usize) -> usize {
+        assert!(x < self.n);
+        self.size[x]
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::UnionFind;
+
+    #[test]
+    fn test() {
+        let mut uf = UnionFind::new(5);
+        uf.merge(1, 2);
+        uf.merge(3, 4);
+        assert!(uf.is_same(1, 2));
+        assert!(uf.is_same(3, 4));
+        assert!(!uf.is_same(0, 1));
+        uf.merge(0, 3);
+        assert!(uf.is_same(0, 3));
+        assert!(!uf.is_same(0, 1));
+    }
+}
