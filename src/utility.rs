3e6c28021172d53c6a54479cf6cae6781e6d3347
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2024-03-12 02:31:12 +0900

    feat: add `rle` which compress data with run-length encoding

--- src/utility.rs
+++ src/utility.rs
@@ -1,5 +1,6 @@
-use std::{collections::BTreeMap, hash::Hash};
+use std::collections::BTreeMap;
 
+/// Perform coordinate compression
 pub fn compress<T: Ord + Clone>(v: Vec<T>) -> Vec<usize> {
     let mut w = v.clone();
     w.sort();
@@ -13,13 +14,42 @@ pub fn compress<T: Ord + Clone>(v: Vec<T>) -> Vec<usize> {
         .collect()
 }
 
+/// Compress given data with run-length encoding
+pub fn rle<T: Eq>(v: Vec<T>) -> Vec<(T, usize)> {
+    let mut iter = v.into_iter();
+    let mut res = Vec::new();
+    let mut prev = if let Some(first) = iter.next() {
+        first
+    } else {
+        return res;
+    };
+    let mut count = 1;
+    for v in iter {
+        if v != prev {
+            res.push((prev, count));
+            count = 0;
+        }
+        count += 1;
+        prev = v;
+    }
+    res.push((prev, count));
+    res
+}
+
 #[cfg(test)]
 mod test {
-    use super::compress;
+    use super::{compress, rle};
 
     #[test]
     fn test_compress() {
         let v = vec![2, 100, 5, 3, 201, 4, 100];
         assert_eq!(compress(v), vec![0, 4, 3, 1, 5, 2, 4]);
+    }
+
+    #[test]
+    fn test_rle() {
+        let v = vec![2, 100, 4, 4, 3, 2, 2, 2];
+        assert_eq!(rle(v), vec![(2, 1), (100, 1), (4, 2), (3, 1), (2, 3)]);
+        assert_eq!(rle::<usize>(vec![]), vec![]);
     }
 }
