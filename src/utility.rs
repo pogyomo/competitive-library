2bf075ff7e8f4c484b9794782930d8a7b3c56867
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2024-03-18 02:08:22 +0900

    feat: add max/min_subsequence for maximum subarray problem

--- src/utility.rs
+++ src/utility.rs
@@ -1,4 +1,8 @@
-use std::collections::BTreeMap;
+use std::{
+    cmp::{max_by, min_by, Ordering},
+    collections::BTreeMap,
+    ops::Add,
+};
 
 /// Perform coordinate compression. Time complexity is O(NlogN).
 pub fn compress<T: Ord + Clone>(v: Vec<T>) -> Vec<usize> {
@@ -35,9 +39,67 @@ pub fn rle<T: Eq, I: IntoIterator<Item = T>>(iter: I) -> Vec<(T, usize)> {
     res
 }
 
+/// Calculate maximum sum of subsequence of given iterator with respect to the specified
+/// comparition function.
+/// Time complexity is O(N).
+pub fn max_subsequence_by<T, I, F>(iter: I, mut compare: F) -> Option<T>
+where
+    T: Ord + Clone + Add<T, Output = T>,
+    I: IntoIterator<Item = T>,
+    F: FnMut(&T, &T) -> Ordering,
+{
+    let mut iter = iter.into_iter();
+    let mut dp = iter.next()?;
+    let mut res = dp.clone();
+    for value in iter {
+        dp = max_by(dp + value.clone(), value, &mut compare);
+        res = max_by(res, dp.clone(), &mut compare);
+    }
+    Some(res)
+}
+
+/// Calculate maximum sum of subsequence of given iterator.
+/// Time complexity is O(N).
+pub fn max_subsequence<T, I>(iter: I) -> Option<T>
+where
+    T: Ord + Clone + Add<T, Output = T>,
+    I: IntoIterator<Item = T>,
+{
+    max_subsequence_by(iter, |a, b| a.cmp(b))
+}
+
+/// Calculate minimum sum of subsequence of given iterator with respect to the specified
+/// comparition function.
+/// Time complexity is O(N).
+pub fn min_subsequence_by<T, I, F>(iter: I, mut compare: F) -> Option<T>
+where
+    T: Ord + Clone + Add<T, Output = T>,
+    I: IntoIterator<Item = T>,
+    F: FnMut(&T, &T) -> Ordering,
+{
+    let mut iter = iter.into_iter();
+    let mut dp = iter.next()?;
+    let mut res = dp.clone();
+    for value in iter {
+        dp = min_by(dp + value.clone(), value, &mut compare);
+        res = min_by(res, dp.clone(), &mut compare);
+    }
+    Some(res)
+}
+
+/// Calculate minimum sum of subsequence of given iterator.
+/// Time complexity is O(N).
+pub fn min_subsequence<T, I>(iter: I) -> Option<T>
+where
+    T: Ord + Clone + Add<T, Output = T>,
+    I: IntoIterator<Item = T>,
+{
+    min_subsequence_by(iter, |a, b| a.cmp(b))
+}
+
 #[cfg(test)]
 mod test {
-    use super::{compress, rle};
+    use super::{compress, max_subsequence, min_subsequence, rle};
 
     #[test]
     fn test_compress() {
@@ -50,5 +112,39 @@ mod test {
         let v = vec![2, 100, 4, 4, 3, 2, 2, 2];
         assert_eq!(rle(v), vec![(2, 1), (100, 1), (4, 2), (3, 1), (2, 3)]);
         assert_eq!(rle(Vec::<usize>::new()), vec![]);
+    }
+
+    #[test]
+    fn test_max_subsequence() {
+        let v = vec![1, -1, 3, -2, 4, -5];
+        let mut max = isize::MIN;
+        for i in 0..v.len() {
+            for j in i..v.len() {
+                let mut sum = 0;
+                for k in i..=j {
+                    sum += v[k];
+                }
+                max = max.max(sum);
+            }
+        }
+        assert_eq!(max_subsequence(v), Some(max));
+        assert_eq!(max_subsequence(Vec::<isize>::new()), None);
+    }
+
+    #[test]
+    fn test_min_subsequence() {
+        let v = vec![1, -1, 3, -2, 4, -5];
+        let mut min = isize::MAX;
+        for i in 0..v.len() {
+            for j in i..v.len() {
+                let mut sum = 0;
+                for k in i..=j {
+                    sum += v[k];
+                }
+                min = min.min(sum);
+            }
+        }
+        assert_eq!(min_subsequence(v), Some(min));
+        assert_eq!(min_subsequence(Vec::<isize>::new()), None);
     }
 }
