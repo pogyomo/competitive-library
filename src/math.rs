9e1703c05649fba2a295286288766168159e4d5d
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2024-03-27 08:00:45 +0900

    feat: add fft module

--- src/math.rs
+++ src/math.rs
@@ -1,9 +1,13 @@
+pub mod fft;
 pub mod theory;
 
-use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Rem, RemAssign, Sub, SubAssign};
+use std::ops::{
+    Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Rem, RemAssign, Sub, SubAssign,
+};
 
 macro_rules! impl_uint {
-    ($($type:ident),* $(,)+) => {$(
+    ($($type:ident),*) => {$(
+        impl NumOps for $type {}
         impl Integral for $type {
             fn zero() -> Self { 0 }
             fn one() -> Self { 1 }
@@ -13,7 +17,8 @@ macro_rules! impl_uint {
 }
 
 macro_rules! impl_int {
-    ($($type:ident),* $(,)+) => {$(
+    ($($type:ident),*) => {$(
+        impl NumOps for $type {}
         impl Integral for $type {
             fn zero() -> Self { 0 }
             fn one() -> Self { 1 }
@@ -22,16 +27,38 @@ macro_rules! impl_int {
     )*};
 }
 
+macro_rules! impl_float {
+    ($($type:ident),*) => {$(
+        impl NumOps for $type {}
+        impl Float for $type {
+            const PI: $type = ::std::$type::consts::PI;
+
+            fn zero() -> Self { 0.0 }
+            fn one() -> Self { 1.0 }
+            fn cos(self) -> Self { self.cos() }
+            fn sin(self) -> Self { self.sin() }
+            // TODO: I just casted usize to f32/f64. Should I return None if the cast lose infomation?
+            fn from_usize(value: usize) -> Self { value as $type }
+        }
+    )*};
+}
+
 impl_uint! {
-    u8, u16, u32, u64, u128, usize,
+    u8, u16, u32, u64, u128, usize
 }
 
 impl_int! {
-    i8, i16, i32, i64, i128, isize,
+    i8, i16, i32, i64, i128, isize
 }
 
-// NOTE: This trait is subject to change. Some functions may be removed or added.
-pub trait Integral:
+impl_float! {
+    f32, f64
+}
+
+// NOTE: These trait is subject to change. Some functions may be removed or added.
+
+/// A collection of math operations.
+pub trait NumOps:
     Add<Self, Output = Self>
     + Sub<Self, Output = Self>
     + Mul<Self, Output = Self>
@@ -42,12 +69,88 @@ pub trait Integral:
     + MulAssign<Self>
     + DivAssign<Self>
     + RemAssign<Self>
-    + Eq
-    + Ord
-    + Copy
     + Sized
 {
+}
+
+/// A trait for integral.
+pub trait Integral: NumOps + Eq + Ord + Copy + Sized {
     fn zero() -> Self;
     fn one() -> Self;
     fn abs(self) -> Self;
+}
+
+/// A trait for floating point number.
+pub trait Float: NumOps + Neg<Output = Self> + PartialEq + PartialOrd + Copy + Sized {
+    const PI: Self;
+
+    fn zero() -> Self;
+    fn one() -> Self;
+    fn cos(self) -> Self;
+    fn sin(self) -> Self;
+    fn from_usize(value: usize) -> Self; // TODO: Create a trait for conversion?
+}
+
+/// A struct represent complex number
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub struct Complex<T> {
+    re: T,
+    im: T,
+}
+
+impl<T> Complex<T> {
+    pub fn new(re: T, im: T) -> Self {
+        Self { re, im }
+    }
+}
+
+impl<T: Float> Complex<T> {
+    pub fn conjugate(self) -> Self {
+        Self::new(self.re, -self.im)
+    }
+}
+
+impl<T: Float> Add for Complex<T> {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.re + rhs.re, self.im + rhs.im)
+    }
+}
+
+impl<T: Float> Sub for Complex<T> {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self.re - rhs.re, self.im - rhs.im)
+    }
+}
+
+impl<T: Float> Mul<T> for Complex<T> {
+    type Output = Self;
+    fn mul(self, rhs: T) -> Self::Output {
+        Self::new(self.re * rhs, self.im * rhs)
+    }
+}
+
+impl<T: Float> Mul for Complex<T> {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self::new(
+            self.re * rhs.re - self.im * rhs.im,
+            self.re * rhs.im + self.im * rhs.re,
+        )
+    }
+}
+
+impl<T: Float> Div<T> for Complex<T> {
+    type Output = Self;
+    fn div(self, rhs: T) -> Self::Output {
+        Self::new(self.re / rhs, self.im / rhs)
+    }
+}
+
+impl<T: Float> Div for Complex<T> {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self::Output {
+        (self * rhs.conjugate()) / (rhs.conjugate() * rhs.conjugate()).re
+    }
 }
---
+++ src/math/fft.rs
@@ -0,0 +1,85 @@
+use super::{Complex, Float};
+use std::iter::zip;
+
+fn fft_impl<T: Float>(a: Vec<Complex<T>>, inv: T) -> Vec<Complex<T>> {
+    assert!(a.len().is_power_of_two());
+    let n = a.len();
+    if n == 1 {
+        return a;
+    }
+
+    let mut f = Vec::with_capacity(n >> 1);
+    let mut g = Vec::with_capacity(n >> 1);
+    for (i, a) in a.into_iter().enumerate() {
+        if i & 1 == 0 {
+            f.push(a);
+        } else {
+            g.push(a);
+        }
+    }
+    let f = fft_impl(f, inv);
+    let g = fft_impl(g, inv);
+
+    let theta = (T::one() + T::one()) * T::PI * inv / T::from_usize(n);
+    let zeta = Complex::new(T::cos(theta), T::sin(theta));
+
+    let mut p = Complex::new(T::one(), T::zero());
+    let mut res = Vec::with_capacity(n);
+    for i in 0..n {
+        res.push(f[i % (n >> 1)] + p * g[i % (n >> 1)]);
+        p = p * zeta;
+    }
+    res
+}
+
+/// Perform fast fourier transform for `a`. We expect the size of `a` is power of two.
+pub fn fft<T: Float>(a: Vec<Complex<T>>) -> Vec<Complex<T>> {
+    fft_impl(a, T::one())
+}
+
+/// Perform inverse fast fourier transform for `a`. We expect the size of `a` is power of two.
+pub fn ifft<T: Float>(a: Vec<Complex<T>>) -> Vec<Complex<T>> {
+    let n = a.len();
+    fft_impl(a, -T::one())
+        .into_iter()
+        .map(|a| a / T::from_usize(n))
+        .collect()
+}
+
+/// Calculate convolution of given two sequences.
+pub fn convolution<T: Float>(mut a: Vec<T>, mut b: Vec<T>) -> Vec<T> {
+    let res_n = a.len() + b.len() - 1;
+    let n = res_n.next_power_of_two();
+    a.resize(n, T::zero());
+    b.resize(n, T::zero());
+    let da = fft(a.into_iter().map(|a| Complex::new(a, T::zero())).collect());
+    let db = fft(b.into_iter().map(|b| Complex::new(b, T::zero())).collect());
+    let dc = zip(da, db).map(|(a, b)| a * b).collect();
+    ifft(dc).into_iter().take(res_n).map(|c| c.re).collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::convolution;
+
+    #[test]
+    fn test_convolution() {
+        let a = vec![1.0, 2.0, 3.0];
+        let b = vec![2.0, 3.0, 4.0];
+        let ab = convolution(a, b)
+            .into_iter()
+            .map(|v: f64| v.round() as usize)
+            .map(|v| v.to_string())
+            .collect::<Vec<_>>();
+        assert_eq!(
+            ab,
+            vec![
+                String::from("2"),
+                String::from("7"),
+                String::from("16"),
+                String::from("17"),
+                String::from("12")
+            ]
+        );
+    }
+}
