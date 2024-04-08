4f8dabe647241805df9442c54e3325a67e180ecb
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2024-04-09 01:55:52 +0900

    feat: return iterator at `vertices`, `adjacents` and `edges`
    We define the `Item` of each iterator as `Cow<'_, ..>` because some
    graph has adjacent of vertex directly, but some graph generate adjacent
    from the vertex. It's good to return reference if we have the item, but
    if we define `Item = &'_ ..`, this doesn't works if the item is created
    inside of method because the reference of created item can't live
    outside of the method. On the other hand, if we define `Item = ..`, it
    will works on both cases, but cloning or copying the already exist item
    is not efficient. So I define `Item = Cow<'_, ..>` so that user can
    switch whether return reference of exist item or created item depende on
    the implementation of graph.

--- src/graph.rs
+++ src/graph.rs
@@ -3,7 +3,9 @@ use std::{
     cmp::Reverse,
     collections::{BTreeMap, BinaryHeap, HashMap, VecDeque},
     hash::Hash,
-    ops::Add,
+    marker::PhantomData,
+    ops::{Add, Range},
+    slice::Iter,
 };
 
 /// A struct which represent a edge in graph.
@@ -19,6 +21,10 @@ impl<V, W> Edge<V, W> {
     pub fn new(from: V, to: V, cost: W) -> Self {
         Self { from, to, cost }
     }
+
+    pub fn as_tuple(&self) -> (&V, &V, &W) {
+        (&self.from, &self.to, &self.cost)
+    }
 }
 
 /// A struct which represent an adjacent vertex.
@@ -33,15 +39,76 @@ impl<V, W> Adjacent<V, W> {
     pub fn new(to: V, cost: W) -> Self {
         Self { to, cost }
     }
+
+    pub fn as_tuple(&self) -> (&V, &W) {
+        (&self.to, &self.cost)
+    }
 }
 
 /// A trait represent directed/undirected graph with/without weight.
+///
+/// # Associated iterators
+///
+/// The `Vertices`, `Adjacents` and `Edges` requires that the type must be iterator and we can't write
+/// such that `type Type = impl Iterator<..>` inside of impl block.
+///
+/// Usually, chained iterator has complicated type and some iterator method take closure so
+/// that writing concrete type for the iterator is difficult or impossible.
+///
+/// One possible solution is define a new struct which hold initial iterator and implement
+/// `Iterator` to it so that desired iterator is created from the initial iterator like below.
+///
+/// ```ignore
+/// struct Wrapper<'a> {
+///     iter: std::slice::Iter<'a, usize>
+/// }
+///
+/// impl<'a> Iterator for Wrapper<'a> {
+///     type Item = (usize, usize);
+///     fn next(&self) -> Self::Item {
+///         // Using map to process iterator's value.
+///         self.iter.next().map(|v| (v, v))
+///     }
+/// }
+/// ```
+///
+/// This method works well, but in competitive programming, this method is too slow for
+/// implementing a custom `Graph`. So the next method stated below is better in such cases.
+///
+/// Next method is boxing the iterator like below.
+///
+/// ```ignore
+/// impl Graph for GraphStruct {
+///     // ...
+///     type Vertices<'a> = Box<dyn Iterator<Item = Cow<'a, Self::V>> + 'a> where Self: 'a;
+///     // ...
+///
+///     // ...
+///     fn vertices(&self) -> Self::Vertices<'_> {
+///         Box::new(todo!() /* write chained iterator here */)
+///     }
+/// }
+/// ```
+///
+/// If we wrap iterator with `Box`, the type will be the style of `Box<dyn Iterator<Item = ...>>`
+/// and it's easy to write. Also, this works even if the iterator using closure.
+///
+/// Boxing iterator take a cost, but if you can accept the cost, it's better.
 pub trait Graph {
     type V: Clone;
     type W: Clone;
+    type Vertices<'a>: Iterator<Item = Cow<'a, Self::V>>
+    where
+        Self: 'a;
+    type Adjacents<'a>: Iterator<Item = Cow<'a, Adjacent<Self::V, Self::W>>>
+    where
+        Self: 'a;
+    type Edges<'a>: Iterator<Item = Cow<'a, Edge<Self::V, Self::W>>>
+    where
+        Self: 'a;
 
-    /// Collect all vertex of this graph.
-    fn vertices(&self) -> Cow<'_, Vec<Self::V>>;
+    /// Iterate all vertex.
+    fn vertices(&self) -> Self::Vertices<'_>;
 
     /// Returns number of vertex in this graph.
     ///
@@ -50,89 +117,113 @@ pub trait Graph {
     /// User should override default implementation if it is possible to return the number of
     /// vertex directly.
     fn vertex_count(&self) -> usize {
-        self.vertices().len()
+        self.vertices().count()
     }
 
-    /// Collect all adjacents of specified vertex.
-    fn adjacents(&self, v: Self::V) -> Cow<'_, Vec<Adjacent<Self::V, Self::W>>>;
+    /// Iterate all adjacent of specified vertex.
+    fn adjacents(&self, v: Self::V) -> Self::Adjacents<'_>;
 
-    /// Collect all edges of this graph.
-    ///
-    /// By default, this call `adjacents` for all vertex gained from `vertices`
-    /// to collect all edges.
-    ///
-    /// User should override default implementation if it is possible to return all edges directly.
-    fn edges(&self) -> Cow<'_, Vec<Edge<Self::V, Self::W>>> {
-        let mut res = Vec::new();
-        for u in self.vertices().iter() {
-            for Adjacent { to: v, cost: w } in self.adjacents(u.clone()).iter().cloned() {
-                res.push(Edge::new(u.clone(), v, w));
-            }
+    /// Iterate all edge.
+    fn edges(&self) -> Self::Edges<'_>;
+}
+
+pub struct AdjacentList<W> {
+    adjs: Vec<Vec<Adjacent<usize, W>>>,
+    edges: Vec<Edge<usize, W>>,
+}
+
+impl<W: Clone> AdjacentList<W> {
+    /// Construct a new `AdjacentList`.
+    pub fn new(n: usize) -> Self {
+        Self {
+            adjs: vec![Vec::new(); n],
+            edges: Vec::new(),
         }
-        Cow::Owned(res)
+    }
+
+    /// Add an edge to graph.
+    pub fn add_edge(&mut self, edge: Edge<usize, W>) {
+        self.adjs[edge.from].push(Adjacent::new(edge.to, edge.cost.clone()));
+        self.edges.push(edge);
     }
 }
 
-impl Graph for Vec<Vec<usize>> {
-    type V = usize;
-    type W = ();
+pub struct AdjacentListVertices<'a> {
+    iter: Range<usize>,
+    _phantom: PhantomData<&'a usize>,
+}
 
-    fn vertices(&self) -> Cow<'_, Vec<Self::V>> {
-        Cow::Owned((0..self.len()).collect())
-    }
-
-    fn vertex_count(&self) -> usize {
-        self.len()
-    }
-
-    fn adjacents(&self, v: Self::V) -> Cow<'_, Vec<Adjacent<Self::V, ()>>> {
-        Cow::Owned(
-            self[v]
-                .iter()
-                .cloned()
-                .map(|v| Adjacent::new(v, ()))
-                .collect(),
-        )
+impl<'a> AdjacentListVertices<'a> {
+    fn new(iter: Range<usize>) -> Self {
+        Self {
+            iter,
+            _phantom: PhantomData,
+        }
     }
 }
 
-impl<W: Clone> Graph for Vec<Vec<(usize, W)>> {
+impl<'a> Iterator for AdjacentListVertices<'a> {
+    type Item = Cow<'a, usize>;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next().map(|v| Cow::Owned(v))
+    }
+}
+
+pub struct AdjacentListEdges<'a, W> {
+    iter: Iter<'a, Edge<usize, W>>,
+}
+
+impl<'a, W> AdjacentListEdges<'a, W> {
+    fn new(iter: Iter<'a, Edge<usize, W>>) -> Self {
+        Self { iter }
+    }
+}
+
+impl<'a, W: Clone> Iterator for AdjacentListEdges<'a, W> {
+    type Item = Cow<'a, Edge<usize, W>>;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next().map(|edge| Cow::Borrowed(edge))
+    }
+}
+
+pub struct AdjacentListAdjacents<'a, W> {
+    iter: Iter<'a, Adjacent<usize, W>>,
+}
+
+impl<'a, W> AdjacentListAdjacents<'a, W> {
+    fn new(iter: Iter<'a, Adjacent<usize, W>>) -> Self {
+        Self { iter }
+    }
+}
+
+impl<'a, W: Clone> Iterator for AdjacentListAdjacents<'a, W> {
+    type Item = Cow<'a, Adjacent<usize, W>>;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next().map(|adj| Cow::Borrowed(adj))
+    }
+}
+
+impl<W: Clone> Graph for AdjacentList<W> {
     type V = usize;
     type W = W;
+    type Vertices<'a> = AdjacentListVertices<'a> where Self: 'a;
+    type Adjacents<'a> = AdjacentListAdjacents<'a, W> where Self: 'a;
+    type Edges<'a> = AdjacentListEdges<'a, W> where Self: 'a;
 
-    fn vertices(&self) -> Cow<'_, Vec<Self::V>> {
-        Cow::Owned((0..self.len()).collect())
+    fn vertices(&self) -> Self::Vertices<'_> {
+        AdjacentListVertices::new(0..self.adjs.len())
     }
 
     fn vertex_count(&self) -> usize {
-        self.len()
+        self.adjs.len()
     }
 
-    fn adjacents(&self, v: Self::V) -> Cow<'_, Vec<Adjacent<Self::V, Self::W>>> {
-        Cow::Owned(
-            self[v]
-                .iter()
-                .cloned()
-                .map(|(v, w)| Adjacent::new(v, w))
-                .collect(),
-        )
-    }
-}
-
-impl<W: Clone> Graph for Vec<Vec<Adjacent<usize, W>>> {
-    type V = usize;
-    type W = W;
-
-    fn vertices(&self) -> Cow<'_, Vec<Self::V>> {
-        Cow::Owned((0..self.len()).collect())
+    fn adjacents(&self, v: Self::V) -> Self::Adjacents<'_> {
+        AdjacentListAdjacents::new(self.adjs[v].iter())
     }
 
-    fn vertex_count(&self) -> usize {
-        self.len()
-    }
-
-    fn adjacents(&self, v: Self::V) -> Cow<'_, Vec<Adjacent<Self::V, Self::W>>> {
-        Cow::Borrowed(&self[v])
+    fn edges(&self) -> Self::Edges<'_> {
+        AdjacentListEdges::new(self.edges.iter())
     }
 }
 
@@ -152,7 +243,8 @@ pub trait BFS: Graph {
         queue.push_back(start.clone());
         dist.set_distance(start, 0);
         while let Some(u) = queue.pop_front() {
-            for Adjacent { to: v, .. } in self.adjacents(u.clone()).iter() {
+            for adj in self.adjacents(u.clone()) {
+                let (v, _) = adj.as_tuple();
                 if dist.distance(v).is_none() {
                     dist.set_distance(v.clone(), dist.distance(&u).unwrap() + 1);
                     queue.push_back(v.clone());
@@ -188,17 +280,14 @@ where
             if dist.distance(&u).map(|d| *d < udist).unwrap_or(false) {
                 continue;
             }
-            for Adjacent {
-                to: v,
-                cost: uvdist,
-            } in self.adjacents(u).iter().cloned()
-            {
-                let vdist = udist.clone() + uvdist;
+            for adj in self.adjacents(u) {
+                let (v, uvdist) = adj.as_tuple();
+                let vdist = udist.clone() + uvdist.clone();
                 if dist.distance(&v).map(|d| *d < vdist).unwrap_or(false) {
                     continue;
                 }
                 dist.set_distance(v.clone(), vdist.clone());
-                pq.push(Reverse((vdist, v)));
+                pq.push(Reverse((vdist, v.clone())));
             }
         }
         dist
@@ -231,15 +320,10 @@ where
         D: SingleSourceDistanceTable<Self::V, Self::W>,
     {
         let n = self.vertex_count();
-        let edges = self.edges();
         dist.set_distance(start, init);
         for i in 0..n {
-            for Edge {
-                from: u,
-                to: v,
-                cost: uvdist,
-            } in edges.iter()
-            {
+            for edge in self.edges() {
+                let (u, v, uvdist) = edge.as_tuple();
                 let Some(udist) = dist.distance(u) else {
                     continue;
                 };
@@ -283,32 +367,33 @@ where
         D: AllPairDistanceTable<Self::V, Self::W>,
         F: FnMut(&Self::W) -> bool,
     {
-        let vs = self.vertices();
-        for i in vs.iter().cloned() {
-            for Adjacent { to: j, cost: w } in self.adjacents(i.clone()).iter().cloned() {
-                dist.set_distance(i.clone(), j, w);
+        for i in self.vertices() {
+            for adj in self.adjacents((*i).clone()) {
+                let (j, w) = adj.as_tuple();
+                dist.set_distance((*i).clone(), j.clone(), w.clone());
             }
-            dist.set_distance(i.clone(), i, init.clone());
+            dist.set_distance((*i).clone(), (*i).clone(), init.clone());
         }
-        for k in vs.iter() {
-            for i in vs.iter() {
-                for j in vs.iter() {
-                    let (Some(dik), Some(dkj)) = (dist.distance(i, k), dist.distance(k, j)) else {
+        for k in self.vertices() {
+            for i in self.vertices() {
+                for j in self.vertices() {
+                    let (Some(dik), Some(dkj)) = (dist.distance(&i, &k), dist.distance(&k, &j))
+                    else {
                         continue;
                     };
                     let new_dij = dik.clone() + dkj.clone();
                     if dist
-                        .distance(i, j)
+                        .distance(&i, &j)
                         .map(|dij| *dij > new_dij)
                         .unwrap_or(true)
                     {
-                        dist.set_distance(i.clone(), j.clone(), new_dij);
+                        dist.set_distance((*i).clone(), (*j).clone(), new_dij);
                     }
                 }
             }
         }
-        for i in vs.iter() {
-            let Some(dii) = dist.distance(i, i) else {
+        for i in self.vertices() {
+            let Some(dii) = dist.distance(&i, &i) else {
                 continue;
             };
             if is_negative(dii) {
@@ -427,12 +512,18 @@ impl<V: Ord, D> SingleSourceDistanceTable<V, D> for BTreeMap<V, D> {
 
 #[cfg(test)]
 mod test {
-    use super::{BellmanFord, Dijkstra, WarshallFloyd, BFS};
+    use crate::graph::Edge;
+
+    use super::{AdjacentList, BellmanFord, Dijkstra, WarshallFloyd, BFS};
 
     #[test]
     fn test_bfs() {
         // test case come from https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_C
-        let graph = vec![vec![1, 3], vec![3], vec![], vec![2]];
+        let mut graph = AdjacentList::new(4);
+        graph.add_edge(Edge::new(0, 1, ()));
+        graph.add_edge(Edge::new(0, 3, ()));
+        graph.add_edge(Edge::new(1, 3, ()));
+        graph.add_edge(Edge::new(3, 2, ()));
         assert_eq!(
             graph.bfs(0, vec![None; 4]),
             vec![Some(0), Some(1), Some(2), Some(1)]
@@ -442,12 +533,12 @@ mod test {
     #[test]
     fn test_dijkstra() {
         // test case come from https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A
-        let graph = vec![
-            vec![(1, 1), (2, 4)],
-            vec![(2, 2), (3, 5)],
-            vec![(3, 1)],
-            vec![],
-        ];
+        let mut graph = AdjacentList::new(4);
+        graph.add_edge(Edge::new(0, 1, 1));
+        graph.add_edge(Edge::new(0, 2, 4));
+        graph.add_edge(Edge::new(1, 2, 2));
+        graph.add_edge(Edge::new(2, 3, 1));
+        graph.add_edge(Edge::new(1, 3, 5));
         assert_eq!(
             graph.dijkstra(0, 0, vec![None; 4]),
             vec![Some(0), Some(1), Some(3), Some(4)]
@@ -457,12 +548,12 @@ mod test {
     #[test]
     fn test_bellman_ford() {
         // test case come from https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_B
-        let graph: Vec<Vec<(usize, i64)>> = vec![
-            vec![(1, 2), (2, 3)],
-            vec![(2, -5), (3, 1)],
-            vec![(3, 2)],
-            vec![],
-        ];
+        let mut graph = AdjacentList::new(4);
+        graph.add_edge(Edge::new(0, 1, 2));
+        graph.add_edge(Edge::new(0, 2, 3));
+        graph.add_edge(Edge::new(1, 2, -5));
+        graph.add_edge(Edge::new(1, 3, 1));
+        graph.add_edge(Edge::new(2, 3, 2));
         assert_eq!(
             graph.bellman_ford(0, 0, vec![None; 4]),
             Some(vec![Some(0), Some(2), Some(-3), Some(-1)])
@@ -472,27 +563,28 @@ mod test {
     #[test]
     fn test_bellman_ford_with_negative_cycle() {
         // test case come from https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_B
-        let graph: Vec<Vec<(usize, i64)>> = vec![
-            vec![(1, 2), (2, 3)],
-            vec![(2, -5), (3, 1)],
-            vec![(3, 2)],
-            vec![(1, 0)],
-        ];
+        let mut graph = AdjacentList::new(4);
+        graph.add_edge(Edge::new(0, 1, 2));
+        graph.add_edge(Edge::new(0, 2, 3));
+        graph.add_edge(Edge::new(1, 2, -5));
+        graph.add_edge(Edge::new(1, 3, 1));
+        graph.add_edge(Edge::new(2, 3, 2));
+        graph.add_edge(Edge::new(3, 1, 0));
         assert_eq!(graph.bellman_ford(0, 0, vec![None; 4]), None);
     }
 
     #[test]
     fn test_warshall_floyd() {
         // test case come from https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_C
-        let graph: Vec<Vec<(usize, i64)>> = vec![
-            vec![(1, 1), (2, 5)],
-            vec![(2, 2), (3, 4)],
-            vec![(3, 1)],
-            vec![(2, 7)],
-        ];
-        let dist = graph.warshall_floyd(0, vec![vec![None; 4]; 4], |w| *w < 0);
+        let mut graph = AdjacentList::new(4);
+        graph.add_edge(Edge::new(0, 1, 1));
+        graph.add_edge(Edge::new(0, 2, 5));
+        graph.add_edge(Edge::new(1, 2, 2));
+        graph.add_edge(Edge::new(1, 3, 4));
+        graph.add_edge(Edge::new(2, 3, 1));
+        graph.add_edge(Edge::new(3, 2, 7));
         assert_eq!(
-            dist,
+            graph.warshall_floyd(0, vec![vec![None; 4]; 4], |w| *w < 0),
             Some(vec![
                 vec![Some(0), Some(1), Some(3), Some(4)],
                 vec![None, Some(0), Some(2), Some(3)],
@@ -505,12 +597,13 @@ mod test {
     #[test]
     fn test_warshall_floyd_with_negative_cycle() {
         // test case come from https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_C
-        let graph: Vec<Vec<(usize, i64)>> = vec![
-            vec![(1, 1), (2, 5)],
-            vec![(2, 2), (3, 4)],
-            vec![(3, 1)],
-            vec![(2, -7)],
-        ];
+        let mut graph = AdjacentList::new(4);
+        graph.add_edge(Edge::new(0, 1, 1));
+        graph.add_edge(Edge::new(0, 2, 5));
+        graph.add_edge(Edge::new(1, 2, 2));
+        graph.add_edge(Edge::new(1, 3, 4));
+        graph.add_edge(Edge::new(2, 3, 1));
+        graph.add_edge(Edge::new(3, 2, -7));
         assert_eq!(
             graph.warshall_floyd(0, vec![vec![None; 4]; 4], |w| *w < 0),
             None
